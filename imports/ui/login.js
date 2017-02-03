781567d6147daecf63fc06e21a189c82f2db9339
Koo <kootg04@gmail.com>
2017-02-03 11:49:02 -0800

    Debug Lines

--- imports/ui/login.js
+++ imports/ui/login.js
@@ -66,6 +66,7 @@ import './login.html'
       return Template.instance().register.get();
     },
     userCount: function() {
+      console.log(Meteor.call('getUserCount'));
       return Meteor.call('getUserCount');
     }
   });
