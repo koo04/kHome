731ab52cb93f3e7587e6320dd8941c0ab8dfeb59
Douglas DeVicariis <kootg04@gmail.com>
2018-02-12 08:29:27 -0800

    Version 2

--- client/main.js
+++
@@ -1,9 +0,0 @@
-import '../imports/ui/body.js';
-import '../imports/ui/torrentList.js';
-import '../imports/ui/torrent.js';
-import '../imports/ui/preload.js';
-import '../imports/ui/weather.js';
-import '../imports/ui/login.js';
-import '../imports/ui/mal.js';
-import '../imports/ui/rss.js';
-
