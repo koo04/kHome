8b1e0e012ed3737147a8cfe609a0a2aebe7f9ab7
Douglas DeVicariis <kootg04@gmail.com>
2018-02-25 21:18:29 -0800

    Debug Data

--- definitions/database.js
+++ definitions/database.js
@@ -5,9 +5,13 @@ let DB = null;
 
 F.wait('database');
 
+console.log(settings.mongodb.url);
+
 MC.connect(settings.mongodb.url, function(err, client) {
 	if (err)
-    throw err;
+		throw err;
+		
+	console.log(client);
     
 	DB = client.db(settings.mongodb.dbName);
 	F.wait('database');
